14a410e3242c16d3ea67ed78e797730eea4b6e22
Edras Pacola <edras.pacola@microchip.com>
2025-12-04 15:33:47 +0100

    lnet tcp interface 1st try

--- mchplnet/interfaces/tcp_ip.py
+++ mchplnet/interfaces/tcp_ip.py
@@ -1,4 +1,6 @@
 """TCP/IP interface implementation for LNet protocol."""
+import logging
+import socket
 
 from mchplnet.interfaces.abstract_interface import Interface
 
@@ -11,7 +13,11 @@ class LNetTcpIp(Interface):
 
     def start(self):
         """Start the TCP/IP interface."""
-        pass
+        try:
+            with self.socket as s:
+                s.connect((self.host, self.port))
+        except TimeoutError as e:
+            logging.debug(e)
 
     def stop(self):
         """Stop the TCP/IP interface."""
@@ -19,12 +25,16 @@ class LNetTcpIp(Interface):
 
     def __init__(self, *args, **kwargs):
         """Initialize the TCP/IP interface."""
-        pass
+        self.port = int(kwargs["port"]) if "port" in kwargs else 12666
+        self.host = kwargs["host"] if "host" in kwargs else "localhost"
+        self.timeout = kwargs["timeout"] if "timeout" in kwargs else 2
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.settimeout(self.timeout)
 
     def write(self, data):
         """Write data to the TCP/IP interface."""
-        pass
+        self.socket.sendall(data)
 
     def read(self):
         """Read data from the TCP/IP interface."""
-        pass
+        self.socket.recv(64)
